574d4024a57bc41ee684d0226ebcbe01e8f7a13c
mhamoudhanafy <mahmoudhanafy92@gmail.com>
2018-06-28 13:16:22 +0200

    test

--- refresh.py
+++ refresh.py
@@ -1,1 +1,88 @@
-print "Hid"
+"""
+Example 1
+"""
+# Define variables
+# myname = 'Mahmoud'
+# Height = 169
+# Weight = 55
+# Sum = Height + Weight
+# print (("Now we are talking about %s") %(myname))
+# print (('He is %d in height and weights %d') %(Height,Weight))
+# response = True
+# print (('is that True?! %r') %(response))
+
+
+"""
+Example 2
+"""
+
+# while True:
+#     Name = raw_input('Enter your name, please: ')
+#     if Name == 'Mahmoud':
+#         print('Ohh yeaaah')
+#         break
+#     else:
+#         print('Try again')
+# print ('Done!')
+
+
+"""
+Example 3
+"""
+# counts = [1,2,3,4,5,6]
+# fruits = ['orange', 'apple', 'banana']
+#
+# for number in counts:
+#     print ('The number is',number)
+# for fruit in fruits:
+#     print ('The fruit is ',fruit)
+#
+# staff = []
+# employee = ['A','B','C']
+# for guy in employee:
+#     print('The employee name is',guy)
+#     staff.append(guy)
+#     print(staff)
+"""
+Example 3
+"""
+# class Restaurant(object):
+#     bankrupt = False
+#     def open_branch(self):
+#         if not self.bankrupt:
+#             print('Open a new branch')
+#         else:
+#             print('Bankrupt!')
+#
+# x = Restaurant()
+# y = Restaurant()
+# x.open_branch()
+# y.bankrupt = True
+# y.open_branch()
+
+"""
+Example 4
+"""
+
+# class TheThing(object):
+#     def __init__(self):
+#         self.number = 0
+#
+#     def some_function(self):
+#         print('I may get a call today')
+#
+#     def add_number(self,more):
+#         self.number += more
+#         print('The updated number is: ',self.number)
+#         return self.number
+#
+# a = TheThing()
+# b = TheThing()
+# a.add_number(20)
+# a.some_function()
+
+
+"""
+Example 5
+"""
+
